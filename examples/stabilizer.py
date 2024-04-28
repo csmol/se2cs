302730065f95dfe33e5d821579405b2dd8383303
Sukhad Joshi <sukhadj1998@gmail.com>
2024-04-28 17:01:49 -0400

    Update stabilizer.py

--- examples/stabilizer.py
+++ examples/stabilizer.py
@@ -1,6 +1,6 @@
 from stabilizer_timelime.src.stabilizer import config
 from stabilizer_timelime.src.stabilizer import attribute_selector, birch_bellwether_p_CFS
-from stabilizer_timelime.src.stabilizer import default_bellwether, find_bellwether, collect_bellwether
+from stabilizer_timelime.src.stabilizer import default_bellwether, find_bellwether
 from stabilizer_timelime.src.stabilizer import performance_calculator, generate_stats_files
 
 config = config.Config(data_path="data_paul/", branching_factor=20)
@@ -9,6 +9,6 @@ attribute_selector(config=config)
 birch_bellwether_p_CFS(config=config)
 default_bellwether(config=config)
 find_bellwether(config)
-collect_bellwether(config)
+
 performance_calculator(config)
 generate_stats_files(config)
